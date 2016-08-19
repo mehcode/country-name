38356b5da044fed352f0a8a8b6fd9035e6aa4a2c
Ryan Leckey <ryan@launchbadge.com>
2016-08-19 01:19:05 -0700

    Allow 2-letter codes

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var DATA = require("./data.json");
 
 function get(countryCode, language) {
+  if (DATA.countryNames[countryCode] != null) {
+    return DATA.countryNames[countryCode];
+  }
+
   var key = DATA.country[countryCode];
   return DATA.countryNames[language][key];
 }
